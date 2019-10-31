1850dfcf63dd1e48c143c0ff23b88933baee6a7d
zhixian001 <okyjh12@naver.com>
2019-11-01 03:00:17 +0900

    fix build script and run script

--- build.sh
+++ build.sh
@@ -6,12 +6,8 @@ if [ ! -d "$PWD/build" ]; then
     mkdir "$PWD/build"
     _TERM_COMMAND="rm -rf $PWD/build ; $_TERM_COMMAND"
 else
-    _TERM_COMMAND="rm -f build/SolidSphere.o ; $_TERM_COMMAND"
-    _TERM_COMMAND="rm -f build/SolidTeapot.o ; $_TERM_COMMAND"
-    _TERM_COMMAND="rm -f build/SolidTorus.o ; $_TERM_COMMAND"
-    _TERM_COMMAND="rm -f build/SolidShape3D.o ; $_TERM_COMMAND"
-    _TERM_COMMAND="rm -f build/Light.o ; $_TERM_COMMAND"
-    _TERM_COMMAND="rm -f build/Material.o ; $_TERM_COMMAND"
+    _TERM_COMMAND="rm -f build/Board.o ; $_TERM_COMMAND"
+    _TERM_COMMAND="rm -f build/Bubble.o ; $_TERM_COMMAND"
 
 
 fi
@@ -27,12 +23,8 @@ fi
 
 
 export LD_LIBRARY_PATH="$PWD/lib:$LD_LIBRARY_PATH"
-g++ -fPIC -Iinclude/ -c src/Light.cpp -o build/Light.o -Wall
-g++ -fPIC -Iinclude/ -c src/Material.cpp -o build/Material.o -Wall
-g++ -fPIC -Iinclude/ -c src/SolidShape3D.cpp -o build/SolidShape3D.o -Wall
-g++ -fPIC -Iinclude/ -c src/SolidTorus.cpp -o build/SolidTorus.o -Wall
-g++ -fPIC -Iinclude/ -c src/SolidTeapot.cpp -o build/SolidTeapot.o -Wall
-g++ -fPIC -Iinclude/ -c src/SolidSphere.cpp -o build/SolidSphere.o -Wall
+g++ -fPIC -Iinclude/ -c src/Bubble.cpp -o build/Bubble.o -Wall
+g++ -fPIC -Iinclude/ -c src/Board.cpp -o build/Board.o -Wall
 
 # g++ -shared -Wl,-soname,libLight.so -o lib/libLight.so build/Light.o 
 # g++ -shared -Wl,-soname,libMaterial.so -o lib/libMaterial.so build/Material.o 
@@ -42,10 +34,10 @@ g++ -fPIC -Iinclude/ -c src/SolidSphere.cpp -o build/SolidSphere.o -Wall
 # g++ -shared -Wl,-soname,libSolidTeapot.so -o lib/libSolidTeapot.so build/SolidTeapot.o build/Material.o build/Light.o build/SolidShape3D.o
 # g++ -shared -Wl,-soname,libSolidSphere.so -o lib/libSolidSphere.so build/SolidSphere.o build/Material.o build/Light.o build/SolidShape3D.o
 
-g++ -shared -Wl,-soname,libSolids.so -o lib/libSolids.so build/SolidSphere.o build/SolidTeapot.o build/SolidTorus.o build/Material.o build/Light.o build/SolidShape3D.o
+g++ -shared -Wl,-soname,libBubblegame.so -o lib/libBubblegame.so build/Board.o build/Bubble.o
 
 
-g++ -Llib -Iinclude/ src/run.cpp -o bin/_run -Wall -lGL -lGLU -lglut -lSolids
+g++ -Llib -Iinclude/ src/run.cpp -o bin/_run -Wall -lGL -lGLU -lglut -lBubblegame
 
 eval "$_TERM_COMMAND"
 
