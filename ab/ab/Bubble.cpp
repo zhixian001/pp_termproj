591cd6b1581b6a7b8edba51f247f96dad81fbc1e
zhixian001 <okyjh12@naver.com>
2019-10-31 19:42:39 +0000

    bubble 주석

--- ab/ab/Bubble.cpp
+++ ab/ab/Bubble.cpp
@@ -1,3 +1,26 @@
+/**
+ * @file Bubble.cpp
+ * @author YHJeon
+ * @brief Bubble Game bubble
+ * @version 0.1
+ * @date 2019-10-30
+ * 
+ * 5종류의 버블이 있고, 변수 c 배열에 RGB 프리셋이 저장되어 있음
+ * 버블
+ * 	반지름: radius
+ * 	현재 좌표: x, y
+ * 	색깔(옵션): option
+ * 
+ * 움직임은 move 멤버로 delta 값 더함
+ * 
+ * 버블을 그리는 함수는 두 종류가 오버로딩 되어있음
+ * 	draw()
+ * 		이미 주어진 option 색으로 버블을 그림
+ *  draw(int color)
+ * 		새로운 색으로 버블 그림
+ * 		아마 game over시 모든 버블의 색을 회색으로 바꾸는 등의 이펙트로 활용 가능할듯
+ * 
+ */
 #include "Bubble.h"
 
 float c[5][3] = { {0.7, 0.0, 0.0}, {0.0, 0.7, 0.0}, {0.0, 0.0, 0.7}, {0.7, 0.7, 0.0}, {0.7, 0.0, 0.7} };
