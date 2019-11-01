d23331e34de09d30d583883790b5db7f606e13af
zhixian001 <okyjh12@naver.com>
2019-11-01 13:36:59 +0900

    add lighting

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ if [ ! -d "$PWD/build" ]; then
     mkdir "$PWD/build"
     _TERM_COMMAND="rm -rf $PWD/build ; $_TERM_COMMAND"
 else
+    _TERM_COMMAND="rm -f build/Light.o ; $_TERM_COMMAND"
     _TERM_COMMAND="rm -f build/Board.o ; $_TERM_COMMAND"
     _TERM_COMMAND="rm -f build/Bubble.o ; $_TERM_COMMAND"
     _TERM_COMMAND="rm -f build/Cannon.o ; $_TERM_COMMAND"
@@ -20,11 +21,12 @@ if [ ! -d "$PWD/bin" ]; then
 fi
 
 export LD_LIBRARY_PATH="$PWD/lib:$LD_LIBRARY_PATH"
+g++ -fPIC -Iinclude/ -c src/Light.cpp -o build/Light.o -Wall
 g++ -fPIC -Iinclude/ -c src/Bubble.cpp -o build/Bubble.o -Wall
 g++ -fPIC -Iinclude/ -c src/Board.cpp -o build/Board.o -Wall
 g++ -fPIC -Iinclude/ -c src/Cannon.cpp -o build/Cannon.o -Wall
 
-g++ -shared -Wl,-soname,libBubblegame.so -o lib/libBubblegame.so build/Board.o build/Bubble.o build/Cannon.o
+g++ -shared -Wl,-soname,libBubblegame.so -o lib/libBubblegame.so build/Board.o build/Bubble.o build/Cannon.o build/Light.o
 
 g++ -Llib -Iinclude/ src/run.cpp -o bin/_run -Wall -lGL -lGLU -lglut -lBubblegame
 
