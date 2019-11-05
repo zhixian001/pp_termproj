66f58fec2c82327994b62c3c38e4353097458ff2
zhixian001 <okyjh12@naver.com>
2019-11-06 03:27:53 +0900

    Text클래스 추가

---
+++ ab/ab/Texts.cpp
@@ -0,0 +1,22 @@
+#include "Texts.h"
+
+Texts::Texts(double _width, double _height) {
+    
+    width = _width;
+    height = _height;
+}
+
+Texts::~Texts()
+{
+}
+
+
+
+
+Txt::Txt(){
+
+}
+
+Txt::~Txt(){
+
+}
