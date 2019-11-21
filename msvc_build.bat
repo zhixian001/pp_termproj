f979a875f4a335a2f3eca3bff0f7e3556798fd7e
JHY <okyjh12@naver.com>
2019-11-21 20:32:32 +0000

    업데이트 .gitlab-ci.yml, msvc_build.bat 파일

--- msvc_build.bat
+++ msvc_build.bat
@@ -1,7 +1,5 @@
 @echo off
 
-REM please execute this file in msvc x86 native compiler shell in current directory
-
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
 
-call msbuild -consoleLoggerParameters:PerformanceSummary;Summary;ShowTimestamp -maxCpuCount:2 -property:Configuration=%BUILD_CONFIG% ab\ab\ab.vcxproj
+call msbuild -consoleLoggerParameters:PerformanceSummary;Summary;ShowTimestamp -maxCpuCount:2 -property:Configuration=%BUILD_CONFIG% ab.vcxproj
