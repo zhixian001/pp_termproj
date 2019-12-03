ff8be8207751d2a520a381d27379459a5a97ec83
zhixian001 <okyjh12@naver.com>
2019-12-03 16:40:14 +0900

    update ignores and builds

--- msvc_build.bat
+++ msvc_build.bat
@@ -2,4 +2,4 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
 
-call msbuild -consoleLoggerParameters:PerformanceSummary;Summary;ShowTimestamp -maxCpuCount:2 -property:Configuration=%BUILD_CONFIG% ab.vcxproj
+call msbuild -consoleLoggerParameters:PerformanceSummary;Summary;ShowTimestamp -maxCpuCount:2 -property:Configuration=%BUILD_CONFIG% Bubblebobble_Termproject_28\Bubblebobble_Termproject_28.vcxproj
